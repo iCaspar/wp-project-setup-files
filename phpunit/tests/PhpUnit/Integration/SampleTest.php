c12844d78c60f0f3b7f0ee748e8a2135a372d785
Caspar Green <cg@caspar.green>
2018-02-20 12:15:01 -0500

    Update Integration/SampleTest.php

--- phpunit/tests/PhpUnit/Integration/SampleTest.php
+++ phpunit/tests/PhpUnit/Integration/SampleTest.php
@@ -7,7 +7,7 @@ namespace ICaspar\Project\Tests\PhpUnit\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class SampleTest extends TestCase {
+class SampleTest extends WP_UnitTestCase {
 
 	public function testTruthy(): void {
 		$this->assertEquals( 1, 1 );
